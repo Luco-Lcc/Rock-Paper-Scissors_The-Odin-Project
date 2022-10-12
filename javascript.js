fc3e6f832e279c756234092fa02f3f088e4c2926
Lcc <lu.calado.costa@gmail.com>
2022-10-12 14:24:14 -0300

    Create function for defining Computer's choice  - Based on random number, Computer's choice will be defined (1 = Rock ; 2 = Paper ; 3 = Scissors);

--- javascript.js
+++ javascript.js
@@ -4,5 +4,19 @@ function getRndInteger() {
     return Math.floor(Math.random() * (3 - 1 + 1)) + 1;
 }
 
-let a = getRndInteger();
-// console.log(a);
+function getComputerChoice() {
+    let pc_number = getRndInteger();
+    
+    if (pc_number===1) {
+        pc_selection = "Rock";
+    }
+
+    if (pc_number===2) {
+        pc_selection = "Paper";
+    }
+
+    if (pc_number===3) {
+        pc_selection = "Scissors";
+    }
+}
+
